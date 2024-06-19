cdd111020330ca80e77aaa54a17f9b612450e316
Felix Zhou <felix.rn.zhou@gmail.com>
2024-06-18 21:47:05 -0400

    home page template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<div class="flex justify-center">
-</div>
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<main class="flex flex-col items-center space-y-20 py-52">
+	<div class="flex flex-col items-center space-y-12">
+		<p class="text-2xl font-semibold">Toronto's Referral Market</p>
+		<p class="text-6xl font-bold"><u>Buy & Sell</u> Referrals For Leading Firms</p>
+		<p>
+			Secure an edge over other job candidates by connecting & collaborating with existing employees
+			in roles you're interested in
+		</p>
+		<Button class="">See Marketplace</Button>
+	</div>
+</main>
