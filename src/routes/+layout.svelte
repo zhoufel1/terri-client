5544c47aa5b786c9dc85bcd3861de162cf1b605e
Felix Zhou <felix.rn.zhou@gmail.com>
2024-06-18 20:57:13 -0400

    add Footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
+	import Footer from './Footer.svelte';
 	import Header from './Header.svelte';
 </script>
 
 <Header/>
 <slot></slot>
+<Footer/>
 
 <style></style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+</script>
+
+<div class="flex flex-col gap-y-10">
+	<Separator />
+	<div class="flex flex-row items-center justify-center space-x-10">
+		<b>© 2024 Terri Corp.</b>
+		<Button variant="link"><u>About Us</u></Button>
+		<Button variant="link"><u>Terms of Service</u></Button>
+		<Button variant="link"><u>Privacy Policy</u></Button>
+		<Button variant="link"><u>Cookie Policy</u></Button>
+	</div>
+</div>
